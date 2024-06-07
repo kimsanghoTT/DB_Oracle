cc9f499de349daa2868e645b2629e62779c8bc16
kimsanghoTT <165999006+kimsanghoTT@users.noreply.github.com>
2024-06-07 17:41:57 +0900

    실습문제

---
+++ SQL코드/99_SQL실습문제5.sql
@@ -0,0 +1,28 @@
+-- 테이블 + 시퀀스 실습문제
+CREATE TABLE USERS (
+    USERID NUMBER NOT NULL,
+    NAME VARCHAR2(50) NOT NULL,
+    EMAIL VARCHAR2(100) NOT NULL,
+    PASSWORD VARCHAR2(100) NOT NULL,
+    CREATEDDATE DATE DEFAULT SYSDATE
+);
+
+CREATE SEQUENCE USER_SEQ;
+INSERT INTO USERS(USERID, NAME, EMAIL, PASSWORD) VALUES (user_seq.nextval, '홍길동', 'hong@example.com', 'password123');
+INSERT INTO USERS(USERID, NAME, EMAIL, PASSWORD) VALUES (user_seq.nextval, '김철수', 'kim@example.com', 'mypassword');
+INSERT INTO USERS(USERID, NAME, EMAIL, PASSWORD) VALUES (user_seq.nextval, '이영희', 'lee@example.com', 'securepass');
+
+CREATE TABLE BOARD(
+    POSTID NUMBER NOT NULL,
+    USERID NUMBER NOT NULL,
+    TITLE VARCHAR2(200) NOT NULL,
+    CONTENT CLOB NOT NULL,
+    CREATEDDATE DATE DEFAULT SYSDATE
+);
+CREATE SEQUENCE BOARD_SEQ;
+INSERT INTO BOARD(POSTID, USERID, TITLE, CONTENT) VALUES(board_seq.nextval, 1, '첫 번째 게시물', '이것은 첫 번째 게시물의 내용입니다.');
+INSERT INTO BOARD(POSTID, USERID, TITLE, CONTENT) VALUES(board_seq.nextval, 2, '두 번째 게시물', '이것은 두 번째 게시물의 내용입니다.');
+INSERT INTO BOARD(POSTID, USERID, TITLE, CONTENT) VALUES(board_seq.nextval, 3, '세 번째 게시물', '이것은 세 번째 게시물의 내용입니다.');
+
+
+
